5f96f4990a8b48c5c4ba796423f98dc280383db2
ayushjha119 <ayush.jha.it23@heritageit.edu.in>
2023-11-13 14:20:12 +0530

    first commit

---
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -0,0 +1,32 @@
+// let js = "amazing";
+// //if (js === "amazing") alert("javascript is fun");
+
+// //40 + 8 - 20 + 23;
+// //we are in script so we write console.log to show in console
+
+// console.log(40 + 45 - 25 + 14);
+
+// // 'Ayush' it is a value
+
+// //console.log("Ayush");
+
+// //value is smallest unit of information
+
+// let firstName = "Ayush";
+
+// let PI = 3.14;
+
+//true;
+// let javascriptIsFun = true;
+// console.log(javascriptIsFun);
+
+// let x = 31;
+// x = 30;
+// console.log(x);
+
+// var y = 221;
+// y = 220;
+// console.log(y);
+
+// let x = "😊";// window plus .(dot)
+// console.log(x);
